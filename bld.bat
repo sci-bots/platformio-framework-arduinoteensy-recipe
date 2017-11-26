a33619841f663f27b7610890d4af4e1816dfec35
Christian Fobel <christian@fobel.net>
2017-11-26 17:12:22 -0500

    feat(wire): bump Wire version to v0.1.2

--- bld.bat
+++ bld.bat
@@ -2,6 +2,7 @@ set PLATFORM_IO_PACKAGE=framework-arduinoteensy
 set PKG_URL=https://dl.bintray.com/platformio/dl-packages/framework-arduinoteensy-1.136.0.tar.gz
 set FILENAME=framework-arduinoteensy-1.136.0.tar.gz
 set PKG_NAME=platformio-framework-arduinoteensy
+set WIRE_VERSION=0.1.2
 
 REM Download Windows binary.
 "%PYTHON%" -m wget "%PKG_URL%"
@@ -15,11 +16,11 @@ REM Delete Wire library
 rmdir /S/Q "%PKG_NAME%\libraries\Wire"
 if errorlevel 1 exit 1
 
-"%PYTHON%" -m wget "https://github.com/sci-bots/Wire/archive/v0.1.1.tar.gz" -o Wire-v0.1.1.tar.gz
+"%PYTHON%" -m wget "https://github.com/sci-bots/Wire/archive/v%WIRE_VERSION%.tar.gz" -o Wire-v%WIRE_VERSION%.tar.gz
 if errorlevel 1 exit 1
-tar xvf Wire-v0.1.1.tar.gz
+tar xvf Wire-v%WIRE_VERSION%.tar.gz
 if errorlevel 1 exit 1
-mv Wire-0.1.1 "%PKG_NAME%\libraries\Wire"
+mv Wire-%WIRE_VERSION% "%PKG_NAME%\libraries\Wire"
 if errorlevel 1 exit 1
 
 REM Copy contents to PlatformIO directory in Conda environment.
